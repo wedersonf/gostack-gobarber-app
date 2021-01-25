f2ffedfb28d739528bbdb02799416a1f5233252c
wedersonf <wedymf@hotmail.com>
2021-01-25 02:08:18 -0300

    header do dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <StatusBar barStyle="light-content" backgroundColor="#28262e" />
     <AppProvider>
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
         <Routes />
