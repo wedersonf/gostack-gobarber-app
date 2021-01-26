e478d1c68abe653846101180f5592f10c1d3602b
wedersonf <wedymf@hotmail.com>
2021-01-25 23:33:48 -0300

    estrutura da tela sucesso

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#28262e" translucent />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
     <AppProvider>
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
         <Routes />
